9d00e49ed804c1763ec53d73ec49ed4e93753571
George Perry <gperry@nd.edu>
2022-11-17 16:51:41 -0500

    Create Signup/Login

--- application/urls.py
+++ application/urls.py
@@ -4,8 +4,12 @@ from . import views
 app_name = 'application'  # creates a namespace for this application
 
 urlpatterns = [
-    # /polls/
-    path('', views.IndexView.as_view(), name='index'),
+    path('', views.index, name='index'),
+    path("signup/", views.SignUp.as_view(), name="signup"),
+    path("login/", views.SignUp.as_view(), name="login"),
+    # path('', views.IndexView.as_view(), name='index'),
+
+
     # /polls/# ex: /polls/1
     # path('<int:pk>/', views.PostDetailView.as_view(), name='view'),
 ]
--- application/views.py
+++ application/views.py
@@ -2,7 +2,19 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
 from .models import Candidate, Recruiter
+
+
+def index(request):
+    return render(request, "application/index.html")
+
+class SignUp(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("application:login")
+    template_name = "application/signup.html"
 
 # Create the index page view - display all posts
 class IndexView(ListView):
