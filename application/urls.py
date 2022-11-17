2868015153ac2153c67bd5806f6096eebaa1b0dd
George Perry <gperry@nd.edu>
2022-11-17 17:22:51 -0500

    Improved Logins

--- application/urls.py
+++ application/urls.py
@@ -6,7 +6,7 @@ app_name = 'application'  # creates a namespace for this application
 urlpatterns = [
     path('', views.index, name='index'),
     path("signup/", views.SignUp.as_view(), name="signup"),
-    path("login/", views.SignUp.as_view(), name="login"),
+    # path("login/", views.Login.as_view(), name="login"),
     # path('', views.IndexView.as_view(), name='index'),
 
 
--- application/views.py
+++ application/views.py
@@ -13,8 +13,8 @@ def index(request):
 
 class SignUp(CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy("application:login")
-    template_name = "application/signup.html"
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
 
 # Create the index page view - display all posts
 class IndexView(ListView):
